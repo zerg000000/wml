cd850ec11657d4ef96e2f4705dc8585ce93d6ce0
albert <kayulai2@gmail.com>
2012-05-01 10:08:56 +0800

    add heroku elasticsearch settings

--- src/wml/core.clj
+++ src/wml/core.clj
@@ -6,7 +6,12 @@
         [wml.routes.main :as main])
   (:require [compojure.handler :as handler]
 	    [ring.middleware.format-response :as resp]
+            [clojurewerkz.elastisch.rest :as rest]
 	    ))
+
+(reset! rest/*endpoint* (ElasticSearchEndpoint. 
+  (get (System/getenv) "BONSAI_INDEX_URL" "http://localhost:9200/")))
+
 (def app 
   (-> (handler/api main/main-routes)
       (wrap-restful-params)
